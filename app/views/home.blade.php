0add6ec39a57389a9cb16d01f7d5c4ffda8a42dc
mastersunny <sunnycse248@gmail.com>
2016-03-11 18:07:11 -0800

    pagination added

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -16,14 +16,8 @@
 
         <div class="row">
           <div class="col s12 m12">
-            <ul class="pagination">
-              <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
-              <li class="active"><a href="#!">1</a></li>
-              <li class="waves-effect"><a href="#!">2</a></li>
-              <li class="waves-effect"><a href="#!">3</a></li>
-              <li class="waves-effect"><a href="#!">4</a></li>
-              <li class="waves-effect"><a href="#!">5</a></li>
-              <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+            <ul class="pagination" id="pagination">
+              
             </ul>
           </div>
         </div>
@@ -52,6 +46,26 @@
         console.log(response);
         
         if(response.code==200){
+
+            var first_page = response.data.from;
+            var last_page = response.data.to;
+            var current_page = response.data.current_page;
+            var pagination = $('#pagination');
+            
+            $('<li class="waves-effect"><a href="' + (first_page-1) + '"><i class="material-icons">chevron_left</i></a></li>').prependTo(pagination);
+
+            for(first_page;first_page<=last_page;first_page++){
+               
+                if(first_page==current_page){
+                   $('<li class="active"><a href="'+ first_page + '">'+ first_page +'</a></li>').appendTo(pagination);
+                }
+                else{
+                  $('<li class="waves-effect"><a href="'+ first_page + '">'+ first_page +'</a></li>').appendTo(pagination);
+                }
+                
+            }
+            $('<li class="waves-effect"><a href="' + (last_page+1) + '"><i class="material-icons">chevron_right</i></a></li>').appendTo(pagination);
+
           $.each( response.data.data, function( i , obj ) {
                makeCards(obj);
            });
@@ -77,18 +91,18 @@
             + '<div class="row">'
             +  '<div class="col s12 m3 card-image">'
             +   '<div class="card-image">'
-            +   '<a href=""><img src="{{asset("site/dhan.JPG")}}"></a>'
+            +   '<a href="' + ('details/' + post.id) +'"><img src="{{asset("site/dhan.JPG")}}"></a>'
             +   ' </div>'  
             +  '</div>'
             +  '<div class="col s12 m9 card-content">'
-            +  '<h5 style="margin:0;">' + post.title + '</h5>'
+            +  '<h5 style="margin:0;"><a href = "' + ('details/'+ post.id) + '">' + post.title + '</a></h5>'
             +  '<hr>'
             +  '<p>' + post.description +  '...</p>'
             + '</div></div>'
             + '<div class="card-action">'
-            + '<a href="#">Uploaded Date :' + post.updated_at + '</a>'
-            + '<a href="#">Seen : 212</a>'
-            + '<a href="#">Uploader : Shafin Sunny</a>'
+            + '<a href="#">Uploaded Date : ' + post.updated_at + '</a>'
+            //+ '<a href="#">Seen : 212</a>'
+           // + '<a href="#">Uploader : Shafin Sunny</a>'
             +  '<div style="float:right;color:#e57373;margin-right:1%;">'
             // + '<i class="small material-icons grade">grade</i>'
             +'</div></div></div>').prependTo('#posts');
--- app/views/site/includes/sideBar.blade.php
+++ app/views/site/includes/sideBar.blade.php
@@ -1,200 +1,60 @@
 <div class="col s12 m3 category">
         
-         <ul class="collapsible" data-collapsible="accordion">
+        <ul class="collapsible" data-collapsible="accordion">
           <li>
-            <div class="collapsible-header"><h5>ঢাকা</h5></div>
-            <div class="collapsible-body">
-               <ul style="text-align:center;padding:1em;">
-                 <li style="color:blue;font-size:130%;">
-                    ধান
-                 </li>
-                 <ul>
-                   <li><a href="#">লবণসহিষ্ঞু ধানের নতুন জাত</a></li>
-                   <li><a href="#">সোনার ধান বাংলামতি</a></li>
-                   <li><a href="#">খরাসহিষ্ণু ধান</a></li>
-                   <li><a href="#">বিনাধান-৮</a></li>
-                   <li><a href="#">বিনাধান-৫</a></li>
-                 </ul>
-               </ul>
-               <ul style="text-align:center;padding:1em;">
-                 <li style="color:blue;font-size:130%;">
-                    তেল জাতীয় ফসল
-                 </li>
-                 <ul>
-                   <li><a href="#">তিল</a></li>
-                   <li><a href="#">সরিষা</a></li>
-                   <li><a href="#">সূর্যমুখী </a></li>
-                   <li><a href="#">সয়াবিন</a></li>
-                 </ul>
-               </ul>
-               <ul style="text-align:center;padding:1em;">
-                 <li style="color:blue;font-size:130%;">
-                    শস্য জাতীয় ফসল
-                 </li>
-                 <ul>
-                   <li><a href="#">আখ</a></li>
-                   <li><a href="#">গম</a></li>
-                   <li><a href="#">চিনাবাদাম</a></li>
-                   <li><a href="#">ভুট্টা</a></li>
-                 </ul>
-               </ul>
+            <div class="collapsible-header center">First</div>
+              <div class="collapsible-body">
+                <ul class="collapsible" data-collapsible="accordion">
+                  <li>
+                    <div class="collapsible-header center">First</div>
+                    <div class="collapsible-body">
+
+                      <div class="collection">
+                        <a href="#!" class="collection-item">Alvin</a>
+                        <a href="#!" class="collection-item active">Alvin</a>
+                        <a href="#!" class="collection-item">Alvin</a>
+                        <a href="#!" class="collection-item">Alvin</a>
+                      </div>
+            
+                    </div>
+                  </li>
+                  <li>
+                    <div class="collapsible-header center">Second</div>
+                    <div class="collapsible-body">
+
+                      <div class="collection">
+                        <a href="#!" class="collection-item">Alvin</a>
+                        <a href="#!" class="collection-item active">Alvin</a>
+                        <a href="#!" class="collection-item">Alvin</a>
+                        <a href="#!" class="collection-item">Alvin</a>
+                      </div>
+            
+                    </div>
+                  </li>
+                  <li>
+                    <div class="collapsible-header center">Third</div>
+                    <div class="collapsible-body">
+                        <div class="collection">
+                        <a href="#!" class="collection-item">Alvin</a>
+                        <a href="#!" class="collection-item active">Alvin</a>
+                        <a href="#!" class="collection-item">Alvin</a>
+                        <a href="#!" class="collection-item">Alvin</a>
+                      </div>
+                    </div>
+                  </li>
+              </ul>
             </div>
           </li>
           <li>
-            <div class="collapsible-header"><h5>রাজশাহী</h5></div>
+            <div class="collapsible-header center">Second</div>
             <div class="collapsible-body">
-               <ul style="text-align:center;padding:1em;">
-                 <li style="color:blue;font-size:130%;">
-                    ধান
-                 </li>
-                 <ul>
-                   <li><a href="#">লবণসহিষ্ঞু ধানের নতুন জাত</a></li>
-                   <li><a href="#">সোনার ধান বাংলামতি</a></li>
-                   <li><a href="#">খরাসহিষ্ণু ধান</a></li>
-                   <li><a href="#">বিনাধান-৮</a></li>
-                   <li><a href="#">বিনাধান-৫</a></li>
-                 </ul>
-               </ul>
-               <ul style="text-align:center;padding:1em;">
-                 <li style="color:blue;font-size:130%;">
-                    তেল জাতীয় ফসল
-                 </li>
-                 <ul>
-                   <li><a href="#">তিল</a></li>
-                   <li><a href="#">সরিষা</a></li>
-                   <li><a href="#">সূর্যমুখী </a></li>
-                   <li><a href="#">সয়াবিন</a></li>
-                 </ul>
-               </ul>
-               <ul style="text-align:center;padding:1em;">
-                 <li style="color:blue;font-size:130%;">
-                    শস্য জাতীয় ফসল
-                 </li>
-                 <ul>
-                   <li><a href="#">আখ</a></li>
-                   <li><a href="#">গম</a></li>
-                   <li><a href="#">চিনাবাদাম</a></li>
-                   <li><a href="#">ভুট্টা</a></li>
-                 </ul>
-               </ul>
             </div>
           </li>
           <li>
-            <div class="collapsible-header"><h5>বরিশাল</h5></div>
+            <div class="collapsible-header center">Third</div>
             <div class="collapsible-body">
-               <ul style="text-align:center;padding:1em;">
-                 <li style="color:blue;font-size:130%;">
-                    ধান
-                 </li>
-                 <ul>
-                   <li><a href="#">লবণসহিষ্ঞু ধানের নতুন জাত</a></li>
-                   <li><a href="#">সোনার ধান বাংলামতি</a></li>
-                   <li><a href="#">খরাসহিষ্ণু ধান</a></li>
-                   <li><a href="#">বিনাধান-৮</a></li>
-                   <li><a href="#">বিনাধান-৫</a></li>
-                 </ul>
-               </ul>
-               <ul style="text-align:center;padding:1em;">
-                 <li style="color:blue;font-size:130%;">
-                    তেল জাতীয় ফসল
-                 </li>
-                 <ul>
-                   <li><a href="#">তিল</a></li>
-                   <li><a href="#">সরিষা</a></li>
-                   <li><a href="#">সূর্যমুখী </a></li>
-                   <li><a href="#">সয়াবিন</a></li>
-                 </ul>
-               </ul>
-               <ul style="text-align:center;padding:1em;">
-                 <li style="color:blue;font-size:130%;">
-                    শস্য জাতীয় ফসল
-                 </li>
-                 <ul>
-                   <li><a href="#">আখ</a></li>
-                   <li><a href="#">গম</a></li>
-                   <li><a href="#">চিনাবাদাম</a></li>
-                   <li><a href="#">ভুট্টা</a></li>
-                 </ul>
-               </ul>
             </div>
           </li>
-          <li>
-            <div class="collapsible-header"><h5>খুলনা</h5></div>
-            <div class="collapsible-body">
-               <ul style="text-align:center;padding:1em;">
-                 <li style="color:blue;font-size:130%;">
-                    ধান
-                 </li>
-                 <ul>
-                   <li><a href="#">লবণসহিষ্ঞু ধানের নতুন জাত</a></li>
-                   <li><a href="#">সোনার ধান বাংলামতি</a></li>
-                   <li><a href="#">খরাসহিষ্ণু ধান</a></li>
-                   <li><a href="#">বিনাধান-৮</a></li>
-                   <li><a href="#">বিনাধান-৫</a></li>
-                 </ul>
-               </ul>
-               <ul style="text-align:center;padding:1em;">
-                 <li style="color:blue;font-size:130%;">
-                    তেল জাতীয় ফসল
-                 </li>
-                 <ul>
-                   <li><a href="#">তিল</a></li>
-                   <li><a href="#">সরিষা</a></li>
-                   <li><a href="#">সূর্যমুখী </a></li>
-                   <li><a href="#">সয়াবিন</a></li>
-                 </ul>
-               </ul>
-               <ul style="text-align:center;padding:1em;">
-                 <li style="color:blue;font-size:130%;">
-                    শস্য জাতীয় ফসল
-                 </li>
-                 <ul>
-                   <li><a href="#">আখ</a></li>
-                   <li><a href="#">গম</a></li>
-                   <li><a href="#">চিনাবাদাম</a></li>
-                   <li><a href="#">ভুট্টা</a></li>
-                 </ul>
-               </ul>
-            </div>
-          </li>
-          <li>
-            <div class="collapsible-header"><h5>সিলেট</h5></div>
-            <div class="collapsible-body">
-               <ul style="text-align:center;padding:1em;">
-                 <li style="color:blue;font-size:130%;">
-                    ধান
-                 </li>
-                 <ul>
-                   <li><a href="#">লবণসহিষ্ঞু ধানের নতুন জাত</a></li>
-                   <li><a href="#">সোনার ধান বাংলামতি</a></li>
-                   <li><a href="#">খরাসহিষ্ণু ধান</a></li>
-                   <li><a href="#">বিনাধান-৮</a></li>
-                   <li><a href="#">বিনাধান-৫</a></li>
-                 </ul>
-               </ul>
-               <ul style="text-align:center;padding:1em;">
-                 <li style="color:blue;font-size:130%;">
-                    তেল জাতীয় ফসল
-                 </li>
-                 <ul>
-                   <li><a href="#">তিল</a></li>
-                   <li><a href="#">সরিষা</a></li>
-                   <li><a href="#">সূর্যমুখী </a></li>
-                   <li><a href="#">সয়াবিন</a></li>
-                 </ul>
-               </ul>
-               <ul style="text-align:center;padding:1em;">
-                 <li style="color:blue;font-size:130%;">
-                    শস্য জাতীয় ফসল
-                 </li>
-                 <ul>
-                   <li><a href="#">আখ</a></li>
-                   <li><a href="#">গম</a></li>
-                   <li><a href="#">চিনাবাদাম</a></li>
-                   <li><a href="#">ভুট্টা</a></li>
-                 </ul>
-               </ul>
-            </div>
-          </li>
-        </ul>
-      </div>
+      </ul>
+
+</div>
